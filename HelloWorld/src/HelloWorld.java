dc1392a3e0ae93fffc8d2d1ea67b84aea8a2dd16
Nata-V <nataliavolkova.by@gmail.com>
2023-03-23 16:51:20 +0300

    HelloWorld

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,15 +1,6 @@
-public class HelloWorld
-{
-    public static void main(String[] args)
-    {
-        int s = 3700;
-        int sec;
-        int m;
-        int min, h;
-        sec =s % 60;
-        m = (s - sec) / 60;
-        min = m % 60;
-        h = (m - min) / 60;
-        System.out.println(h+" часов " + min + " минут " + sec + " секунд");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Belarus! I like Java!");
     }
+
 }

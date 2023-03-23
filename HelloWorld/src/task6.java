58691836e7d723acf19b2cd2990c7938897bcc0c
Nata-V <nataliavolkova.by@gmail.com>
2023-03-23 17:48:56 +0300

    task4

--- HelloWorld/src/task6.java
+++ HelloWorld/src/task6.java
@@ -2,14 +2,18 @@ public class task6
 {
     public static void main(String[] args)
     {
-        int s = 3700;
+        int s = 3700000;
         int sec;
         int m;
         int min, h;
+        int day;
+        int week;
         sec =s % 60;
         m = (s - sec) / 60;
         min = m % 60;
         h = (m - min) / 60;
-        System.out.println(h+" часов " + min + " минут " + sec + " секунд");
+        day = h/24;
+        week =  day/7;
+        System.out.println(h+" часов " + min + " минут " + sec + " секунд " + day + " дней " + week + " недель ");
     }
 }

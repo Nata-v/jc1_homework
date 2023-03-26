1212bd560da78111ff04786700a77714c7851fc3
Nata-V <nataliavolkova.by@gmail.com>
2023-03-26 21:14:20 +0300

    task4

---
+++ HelloWorld/src/lesson5/Conditional1/Example.java
@@ -0,0 +1,13 @@
+package lesson5.Conditional1;
+
+public class Example {
+    public static void main(String[] args) {
+        int x = 5, y = 7;
+        if (x == 5) {
+            y = 9;
+        }
+        if (y == 9) {
+            x = 3;
+        }
+    }
+}
--- HelloWorld/src/task5.java
+++ HelloWorld/src/task5.java
@@ -1,5 +1,14 @@
 public class task5 {
     public static void main(String[] args) {
+        int a = 7;
+        int b = 5;
+        int c = a + b;
+        int d = a * b;
 
+        System.out.print(sumNumbers(c, d));
+    }
+    public static int sumNumbers(int c, int d) {
+        int result = c + d;
+        return result;
     }
 }
